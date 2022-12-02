069df82ea6889ad75b393b5e4103f8d5899cd6f4
Samuel Erdö <e12022482@student.tuwien.ac.at>
2022-12-02 15:37:24 +0100

    better logging

--- Dayxx.cpp
+++ Dayxx.cpp
@@ -2,23 +2,28 @@
 
 namespace dayxx
 {
-	auto logic1(std::string file)
+	auto logic1(std::string file, bool debug = false)
 	{
 		return -1;
 	}
 
-	auto logic2(std::string file)
+	auto logic2(std::string file, bool debug = false)
 	{
 		return -1;
 	}
 
 	void runTest()
 	{
+		bool debug{true};
+
 		std::string file{""};
+		std::cout << "*** Testing day xx ***\n\n";
 
 		auto x = logic1(file);
 		std::cout << "Part 1 (Test): " << x << "\n";
 		assert(x == dayxx::RESULT1);
+
+		if (debug) std::cout << "------------------" << "\n";
 
 		auto y = logic2(file);
 		std::cout << "Part 2 (Test): " << y << "\n";
@@ -28,6 +33,7 @@ namespace dayxx
 	void run()
 	{
 		std::string file{""};
+		std::cout << "*** Running day xx ***\n\n";
 
 		auto x = logic1(file);
 		std::cout << "Part 1: " << x << "\n";
--- Dayxx.h
+++ Dayxx.h
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <vector>
 #include <cassert>
 
 #ifndef Dayxx
@@ -13,8 +14,8 @@ namespace dayxx
 	constexpr int RESULT1{-1};	   // part 1 example result
 	constexpr int RESULT2{-1};     // part 2 example result
 
-	auto logic1(std::string file); // for part 1
-	auto logic2(std::string file); // for part 1
+	auto logic1(std::string file, bool debug); // for part 1
+	auto logic2(std::string file, bool debug); // for part 1
 	void runTest();                // for testing (example input)
 	void run();                    // for calculating (real input)
 }
