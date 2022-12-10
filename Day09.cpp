bd60fafcc8b1352283223d680929d49dff2198d1
Samuel Erdö <e12022482@student.tuwien.ac.at>
2022-12-10 03:15:13 +0100

    day 9 part 1

--- Day09.cpp
+++ Day09.cpp
@@ -2,18 +2,85 @@
 
 namespace day09
 {
+	struct Position
+	{
+		int x{0};
+		int y{0};
+
+		bool operator<(const Position& r) const noexcept
+		{
+			return (x == r.x) ? (y < r.y) : (x < r.x);
+		}
+
+		std::string to_string()
+		{
+			return "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
+		}
+	};
+
+	using Visited = std::set<Position>;
+
+	int distance(Position& head, Position& tail)
+	{
+		return std::sqrt(std::pow(head.x - tail.x, 2) + std::pow(head.y - tail.y, 2));
+	}
+
+	void setPosition(Position& position, Position& newPos)
+	{
+		position.x = newPos.x; position.y = newPos.y;
+	}
+
+	void moveHead(Position& head, const char direction)
+	{
+		switch (direction)
+		{
+			case 'L': --head.x; break;
+			case 'R': ++head.x; break;
+			case 'D': --head.y; break;
+			case 'U': ++head.y; break;
+		}
+	}
+
+	void moveTail(Position& head, Position& tail, Position& last, Visited& visited, const char direction)
+	{
+		if (distance(head, tail) <= 1) return;
+		setPosition(tail, last);
+		visited.insert(tail);
+	}
+
+	void move(
+		Position& head,
+		Position& tail,
+		Position& last,
+		Visited& visited,
+		const char direction,
+		const int steps
+	)
+	{
+		for (int i{0}; i < steps; ++i)
+		{
+			setPosition(last, head);                        // needed for correct tail-movement
+			moveHead(head, direction);                      // move head 1-step into direction
+			moveTail(head, tail, last, visited, direction); // move tail to last position if needed
+		}
+	}
+
 	auto logic1(std::string file, bool debug = false)
 	{
 		std::ifstream stream{file};
 		if (!stream.is_open()) return -1;
 
 		std::string line;
+		Position head, tail, last;
+		Visited visited{{0,0}}; // inital tail position needs to be added, otherwise wont count
 		while (std::getline(stream, line))
 		{
-
+			char direction{line.substr(0,1)[0]};
+			int steps{std::stoi(line.substr(2))};
+			move(head, tail, last, visited, direction, steps);
 		}
 
-		return -1;
+		return (int)visited.size();
 	}
 
 	auto logic2(std::string file, bool debug = false)
@@ -34,7 +101,7 @@ namespace day09
 	{
 		bool debug{true};
 
-		std::string file{""};
+		std::string file{"Day09_Test.txt"};
 		std::cout << "*** Testing day 09 ***\n\n";
 
 		auto x = logic1(file);
@@ -50,7 +117,7 @@ namespace day09
 
 	void run()
 	{
-		std::string file{""};
+		std::string file{"Day09_Input.txt"};
 		std::cout << "*** Running day 09 ***\n\n";
 
 		auto x = logic1(file);
--- Main.cpp
+++ Main.cpp
@@ -11,7 +11,7 @@
 
 int main()
 {
-	int day{8}, test{1};
+	int day{9}, test{0};
 
 	if (test) {
 		if (day < 1)  dayxx::runTest();
