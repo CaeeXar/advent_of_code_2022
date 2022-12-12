2cdabd657afb0aab7b12d98047945623623874b1
Samuel Erdö <e12022482@student.tuwien.ac.at>
2022-12-12 17:22:36 +0100

    day 11 part 1

--- Day11.cpp
+++ Day11.cpp
@@ -2,32 +2,205 @@
 
 namespace day11
 {
-	auto logic1(std::string file, bool debug = false)
-	{
-		std::ifstream stream{file};
-		if (!stream.is_open()) return -1;
+#pragma region PART1
+	using WorryLevels = std::vector<unsigned long long>;
 
-		std::string line;
-		while (std::getline(stream, line))
+	struct Monkey
+	{
+		const int ID{0};
+		WorryLevels worryLevels{};
+		const std::string operand{""};
+		const char operation{'+'};
+		const unsigned long long test{0};
+		const int trueMonkey{0};
+		const int falseMonkey{0};
+		unsigned long long inspected{0};
+
+		unsigned long long newWorryLevel(const unsigned long long old)
 		{
-			std::cout << line << "\n";
+			unsigned long long newValue{0};
+			switch (operation)
+			{
+				case '+': newValue = old + (operand == "old" ? old : std::stoull(operand));
+					break;
+				case '*': newValue = old * (operand == "old" ? old : std::stoull(operand));
+					break;
+			}
+
+			return newValue;
 		}
 
-		return -1;
+		std::string to_string()
+		{
+			std::string s = "Monkey " + std::to_string(ID) + ":\n" +
+				std::string(" - Items: ");
+
+			for (const unsigned long long& worryLevel : worryLevels) s += std::to_string(worryLevel) + ", ";
+			s += std::string("\n") + std::string(" - Operation: new = old ") +
+				(char)operation + std::string(" ") + operand + std::string("\n") +
+				std::string(" - Test: divisible by ") + std::to_string(test) + std::string("\n") +
+				std::string("    - If true: throw to monkey ") + std::to_string(trueMonkey) +
+				std::string("\n") + std::string("    - If false: throw to monkey ") +
+				std::to_string(falseMonkey) + std::string("\n");
+			return s;
+
+		}
+
+	};
+
+	using Monkeys = std::vector<Monkey>;
+
+	Monkey& getMonkeyByID(Monkeys& monkeys, const int ID)
+	{
+		Monkey nll;
+		for (Monkey& m : monkeys)
+		{
+			if (m.ID == ID) return m;
+		}
+
+		return nll;
+	}
+
+	Monkeys initializeMonkeys(std::string file)
+	{
+		std::ifstream stream{file};
+		if (!stream.is_open()) return {};
+
+		Monkeys monkeys{};
+		WorryLevels worryLevels;
+		std::string line, operand{""};
+		char operation{'+'};
+		int ID{0}, trueMonkey{0}, falseMonkey{0};
+		unsigned long long test{0};
+		std::vector<std::string> temp;
+		while (std::getline(stream, line))
+		{
+			if (line == "")
+			{
+				monkeys.push_back({ID++, worryLevels, operand, operation, test, trueMonkey, falseMonkey});
+				worryLevels = {};
+				continue;
+			}
+
+			if (boost::starts_with(line, "  Starting items:"))
+			{
+				boost::split(temp, line.substr(18), boost::is_any_of(","), boost::token_compress_on);
+				for (std::string item : temp) worryLevels.push_back(std::stoull(item));
+			}
+
+			if (boost::starts_with(line, "  Operation:"))
+			{
+				boost::split(temp, line.substr(23), boost::is_any_of(" "), boost::token_compress_on);
+				operation = {temp[0][0]};
+				operand = {temp[1]};
+			}
+
+			if (boost::starts_with(line, "  Test:")) test = {std::stoull(line.substr(21))};
+			if (boost::starts_with(line, "    If true:")) trueMonkey = {std::stoi(line.substr(29))};
+			if (boost::starts_with(line, "    If false:")) falseMonkey = {std::stoi(line.substr(30))};
+		}
+
+		monkeys.push_back({ID++, worryLevels, operand, operation, test, trueMonkey, falseMonkey});
+
+		return monkeys;
+	}
+
+	void playRound(Monkeys& monkeys, const unsigned long long divider = 3ull)
+	{
+		WorryLevels newWorryLevels{};
+		for (Monkey& monkey : monkeys)
+		{
+			while (monkey.worryLevels.size() > 0)
+			{
+				unsigned long long newWorryLevel{static_cast<unsigned long long>(
+					(monkey.newWorryLevel(monkey.worryLevels[0]) / divider)
+				)};
+
+				if (newWorryLevel <= 0)
+				{
+					std::cout << "current WL: " << monkey.worryLevels[0] << "\n";
+					std::cout << "new WL    : " << newWorryLevel << "\n";
+				}
+
+				monkey.worryLevels.erase(monkey.worryLevels.begin());
+				Monkey& m{getMonkeyByID(monkeys,
+					(newWorryLevel % monkey.test == 0) ? monkey.trueMonkey : monkey.falseMonkey)};
+				m.worryLevels.push_back(newWorryLevel);
+				monkey.inspected++;
+			}
+
+		}
+	}
+
+	void printMonkeys(const Monkeys& monkeys)
+	{
+		for (Monkey monkey : monkeys)
+		{
+			std::cout << monkey.to_string() << "\n";
+		}
+	}
+
+	void printInspected(const Monkeys& monkeys)
+	{
+		for (Monkey monkey : monkeys)
+		{
+			std::cout << "Monkey " << monkey.ID << " inspected items "
+				<< monkey.inspected << " times!\n";
+		}
+	}
+#pragma endregion PART1
+
+	auto logic1(std::string file, bool debug = false)
+	{
+		// play 20 rounds
+		Monkeys monkeys{initializeMonkeys(file)};
+		for (unsigned long long i{0}; i < 20; ++i)
+		{
+			playRound(monkeys);
+		}
+
+		if (debug) printMonkeys(monkeys);
+
+		// get 2 most active monkeys 
+		std::set<unsigned long long> max{};
+		for (Monkey m : monkeys)
+		{
+			max.insert(m.inspected);
+		}
+
+		auto end = max.end();
+		unsigned long long x = {*--end}, y = {*--end};
+		return x * y;
 	}
 
 	auto logic2(std::string file, bool debug = false)
 	{
-		std::ifstream stream{file};
-		if (!stream.is_open()) return -1;
-
-		std::string line;
-		while (std::getline(stream, line))
+		// play 10000 rounds
+		unsigned long long divider{1};
+		Monkeys monkeys{initializeMonkeys(file)};
+		for (int i{0}; i < 10000; ++i)
 		{
-
+			playRound(monkeys, divider);
+			if ((i + 1) % 1000 == 0)
+			{
+				std::cout << "== After round " << (i + 1) << " ==\n";
+				printInspected(monkeys);
+				std::cout << "\n";
+			}
 		}
 
-		return -1;
+		if (debug) printMonkeys(monkeys);
+
+		// get 2 most active monkeys 
+		std::set<unsigned long long> max{};
+		for (Monkey m : monkeys)
+		{
+			max.insert(m.inspected);
+		}
+
+		auto end = max.end();
+		unsigned long long x = {*--end}, y = {*--end};
+		return x * y;
 	}
 
 	void runTest()
--- Day11.h
+++ Day11.h
@@ -1,9 +1,12 @@
+#include <boost/algorithm/string.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <vector>
 #include <cassert>
+#include <set>
 
 #ifndef Day11
 
@@ -11,13 +14,13 @@
 
 namespace day11
 {
-	constexpr int RESULT1{-1};	   // part 1 example result
-	constexpr int RESULT2{-1};     // part 2 example result
+	constexpr long long RESULT1{10605};		   // part 1 example result
+	constexpr long long RESULT2{2713310158};   // part 2 example result
 
 	auto logic1(std::string file, bool debug); // for part 1
 	auto logic2(std::string file, bool debug); // for part 1
-	void runTest();                // for testing (example input)
-	void run();                    // for calculating (real input)
+	void runTest();                            // for testing (example input)
+	void run();								   // for calculating (real input)
 }
 
 #endif
